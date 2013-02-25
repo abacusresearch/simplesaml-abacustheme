7783ba4be7296c098d5bc4c5aa3bff996a646019
Aji Yahya <aji.yahya@abacus.ch>
2013-02-25 11:44:10 +0100

    Initial version

---
+++ themes/abacustheme/default/includes/footer.php
@@ -0,0 +1,31 @@
+<?php
+
+
+
+if(!empty($this->data['htmlinject']['htmlContentPost'])) {
+	foreach($this->data['htmlinject']['htmlContentPost'] AS $c) {
+		echo $c;
+	}
+}
+
+
+?>
+
+
+
+		</div>
+			
+			<div id="footerfunction">
+				<span></span>		
+				<span></span>		
+				<span></span>		
+			</div>
+
+			
+		</div>
+		<div id="footer-section"><div> ©&nbsp;2013&nbsp;ABACUS Research AG  &#124;  Abacus-Platz 1 - 9300 Wittenbach-St.Gallen, Schweiz  <span class="print-hidden">-  <a href="tel:0041712922525">Tel: +41 (0)71 292 25 25</a>  -  Fax: +41 (0)71 292 25 00  -  <a href="mailto:info@abacus.ch">info@abacus.ch</a></span></div></div>
+	</div>
+</div>
+
+</body>
+</html>
