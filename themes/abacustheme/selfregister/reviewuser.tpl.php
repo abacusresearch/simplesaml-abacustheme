312f54b9ee1059f41f1f9d1a086cd279eccb9126
Philipp Hug <philipp.hug@abacus.ch>
2013-07-02 15:54:08 +0200

    moved into selfregister module

--- themes/abacustheme/selfregister/reviewuser.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$this->data['header'] = $this->t('{selfregister:selfregister:link_review}');
-$this->data['head'] = '<link rel="stylesheet" href="resources/selfregister.css" type="text/css">';
-
-$this->includeAtTemplateBase('includes/header.php'); ?>
-
-
-<?php $this->includeAtTemplateBase('includes/footer.php'); ?>
